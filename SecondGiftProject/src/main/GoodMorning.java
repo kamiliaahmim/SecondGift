23bb2d4749567b7837d2711cc2ea9551f2c5c05a
kamiliaahmim <kamiliaahmim@192.168.0.8>
2018-03-10 23:27:06 -0500

    trying

---
+++ SecondGiftProject/src/main/GoodMorning.java
@@ -0,0 +1,14 @@
+package main;
+
+public class GoodMorning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		
+		System.out.println("Good Morning");
+		
+		System.out.println("you guys need some coffee?");
+	}
+
+}
